99c1c3de2ed2f9e73df768990363810636454504
itaygera <itaygera@gmail.com>
2020-02-22 11:59:24 +0200

    Fix indentation and add prints

--- notify_ip_change.py
+++ notify_ip_change.py
@@ -1,4 +1,4 @@
-from time import sleep
+from time import sleep, localtime
 import requests
 import smtplib
 from email.message import EmailMessage
@@ -17,7 +17,9 @@ def send_mail(my_ip):
     msg['To'] = send_to
 
     # Send the message via our own SMTP server.
+    print("Connecting to SMTP server")
     s = smtplib.SMTP('localhost:8025')
+    print(f'Sending email to {send_to}, from {send_from}')
     s.send_message(msg=msg)
     s.quit()
 
@@ -26,13 +28,21 @@ def main():
     my_ip = ""
 
     while True:
+        ltime = localtime()
         resp = requests.get(url=url)
         cur_ip = resp.text.strip()
-    if my_ip != cur_ip:
-        my_ip = cur_ip
-        send_mail(my_ip)
-    sleep(time_between_checks)
+        timestamp = f'{ltime.tm_mday}/{ltime.tm_mon}/{ltime.tm_year}'
+        timestamp += f' {ltime.tm_hour}:{ltime.tm_min}'
+        print(f'[{timestamp}] the cur ip is {cur_ip}')
+        if my_ip != cur_ip:
+            print('The ip address have been changed')
+            print(f'The old ip was {my_ip}, and the new is {cur_ip}')
+            my_ip = cur_ip
+            send_mail(my_ip)
+        sleep(time_between_checks)
 
 
 if __name__ == "__main__":
+    print("Start")
     main()
+    print("End")
